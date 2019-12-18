75410005915bb54041a6a497b70834d48adc5c05
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-12-18 08:37:12 +0100

    cleaned branch

--- src/electron/providers/ipcHandler.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ipcHandler = /** @class */ (function () {
-    function ipcHandler() {
-    }
-    return ipcHandler;
-}());
-exports.default = ipcHandler;
-//# sourceMappingURL=ipcHandler.schema.js.map
--- src/environments/environment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppConfig = {
-    production: false,
-    environment: 'LOCAL'
-};
-//# sourceMappingURL=environment.js.map
