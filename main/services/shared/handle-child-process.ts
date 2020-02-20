9bd57b9188792e14e8520a046019b5015edbfafb
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2020-02-20 10:15:59 +0100

    WIP: addind service to retrieve commands of a distribution

---
+++ main/services/shared/handle-child-process.ts
@@ -0,0 +1,22 @@
+import { ChildProcessWithoutNullStreams } from "child_process";
+
+export function promiseChildProcess(childProcess: ChildProcessWithoutNullStreams): Promise<{}> {
+    return new Promise((resolve, reject) => {
+      let result = '';
+      let error = '';
+      childProcess.stdout.on('data', (data) => {
+        result += data;
+      });
+      childProcess.stderr.on('data', (data) => {
+        error += data;
+      });
+      childProcess.on('close', () => {
+        if (error) {
+          reject(result + error);
+        } else {
+          resolve(result + error);
+        }
+      });
+      childProcess.stdin.end();
+    });
+  }
