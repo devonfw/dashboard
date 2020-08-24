8ab7ed8be3ae543a26ff0283ae503e8bdd6cd719
Juan Andrés Ambuludi <juan-andres.ambuludi-olmedo@capgemini.com>
2020-08-24 11:16:05 +0200

    fixed error handler for projects creation

---
+++ main/modules/projects/classes/listeners/listener-error-handler.ts
@@ -0,0 +1,25 @@
+const MIN_CONSECUTIVE_SUCCESS = 2;
+
+export class ListenerErrorHandler {
+  successCount: number;
+
+  constructor() {
+    this.successCount = 0;
+  }
+
+  public setError(): void {
+    this.successCount = 0;
+  }
+
+  public setSuccess(): void {
+    this.successCount++;
+  }
+
+  public getStatus(): string {
+    if (this.successCount >= MIN_CONSECUTIVE_SUCCESS) {
+      return '';
+    }
+
+    return 'error';
+  }
+}
