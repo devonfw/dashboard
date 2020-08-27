037bd6ac78bc2cae4fc0fc2cbe85fee18b698511
chdattat <chillal.dattatreya@capgemini.com>
2020-08-27 17:01:04 +0530

    removed unused files which i have created

--- renderer/components/dashboard-projects/dashboard-filter/dashboard-filter.styles.tsx
+++
@@ -1,9 +0,0 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-
-export const useDashboardFilterStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    filterSelection: {
-      width: '20em',
-    },
-  })
-);
