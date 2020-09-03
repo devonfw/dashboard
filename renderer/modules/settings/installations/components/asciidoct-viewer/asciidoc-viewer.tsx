9ced37f0820ac94b24655ecc5583b0be88ccf0d8
Juan Andrés Ambuludi <juan-andres.ambuludi-olmedo@capgemini.com>
2020-09-03 13:29:00 +0200

    fix links opening inside electron

--- renderer/modules/settings/installations/components/asciidoct-viewer/asciidoc-viewer.tsx
+++ renderer/modules/settings/installations/components/asciidoct-viewer/asciidoc-viewer.tsx
@@ -1,8 +1,25 @@
 import React, { HTMLAttributes } from 'react';
 import Asciidoctor from 'asciidoctor';
+import LinkOpenerService from '../../../../shared/services/link-opener/link-opener.service';
 
 interface AsciidocViewerProps {
   content: string;
+}
+
+function handleOpenInBrowser(parent: HTMLDivElement) {
+  const linkOpener = new LinkOpenerService();
+  parent.addEventListener('click', (event: MouseEvent) => {
+    if (!event.target) {
+      return;
+    }
+
+    const clicked = event.target as HTMLElement;
+    if (clicked.nodeName == 'A') {
+      const anchorTag = clicked as HTMLAnchorElement;
+      event.preventDefault();
+      linkOpener.openLink(anchorTag.href);
+    }
+  });
 }
 
 export default function AsciidocViewer(
@@ -10,6 +27,11 @@ export default function AsciidocViewer(
 ): JSX.Element {
   return (
     <div
+      ref={(elem) => {
+        if (elem) {
+          handleOpenInBrowser(elem);
+        }
+      }}
       {...props}
       dangerouslySetInnerHTML={{
         __html: Asciidoctor().convert(props.content) as string,
