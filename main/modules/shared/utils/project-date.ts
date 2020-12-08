92dbbff682ceb567d68f2dfd3b6053cdf32cd011
Saptarshi <saptarshi27@gmail.com>
2020-12-08 17:03:27 +0530

    #97: Corrected date (month) capture on project creation

--- main/modules/shared/utils/project-date.ts
+++ main/modules/shared/utils/project-date.ts
@@ -1,7 +1,7 @@
 export function projectDate(): string {
   const currentDate = new Date();
   const date = currentDate.getDate();
-  const month = currentDate.getMonth();
+  const month = currentDate.getMonth() + 1;
   const year = currentDate.getFullYear();
 
   return `${date}/${month}/${year}`;
