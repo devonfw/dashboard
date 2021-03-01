9551e52c1838c961baf3be0c561f59c5bfa9869f
Saptarshi <saptarshi27@gmail.com>
2021-03-01 17:16:58 +0530

    for testing on github

--- renderer/modules/home/components/welcome-to-devonfw/welcome-to-devonfw.spec.ts
+++ renderer/modules/home/components/welcome-to-devonfw/welcome-to-devonfw.spec.ts
@@ -1,3 +1,18 @@
+import { unmountComponentAtNode } from 'react-dom';
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
 describe('Addition', () => {
   it('knows that 2 and 2 make 4', () => {
     expect(2 + 2).toBe(4);
